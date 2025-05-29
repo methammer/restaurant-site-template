80b19bcde5ffb5e8686e77f3813db4a6a5d14c5f
methammer <methammer@gmail.com>
2025-05-29 21:34:02 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,25 @@ import AboutPage from './pages/AboutPage';
 import ReservationsPage from './pages/ReservationsPage';
 import GalleryPage from './pages/GalleryPage';
 import ContactPage from './pages/ContactPage';
-import { ThemeProvider } from "@/components/theme-provider"; // Import ThemeProvider
+// Supprimez l'import de ThemeProvider ici s'il n'est plus utilisé ailleurs dans ce fichier après le retrait.
+// import { ThemeProvider } from "@/components/theme-provider"; 
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/menu" element={<MenuPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/reservations" element={<ReservationsPage />} />
-            <Route path="/gallery" element={<GalleryPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            {/* Add other routes here */}
-          </Routes>
-        </Layout>
-      </Router>
-    </ThemeProvider>
+    // Le ThemeProvider a été retiré d'ici
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/reservations" element={<ReservationsPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          {/* Add other routes here */}
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
